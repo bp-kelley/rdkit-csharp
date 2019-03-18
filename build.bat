e44ff6491f12987c373eece4cda00ba30c7d1b7f
Brian Kelley <fustigator@gmail.com>
2019-03-18 07:11:29 -0400

    Build statically

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 set THISDIR=%~dp0
 echo %THISDIR%
 set PACKAGE=%THISDIR%Nuget.Local
-set BOOST_INCLUDEDIR=%PACKAGE%/boost.1.62.0.0/lib/native/include
+set BOOST_INCLUDEDIR=%PACKAGE%/boost.1.69.0.0/lib/native/include
 set BOOST_LIBRARYDIR=%PACKAGE%/lib
 set EIGEN3_INCLUDE_DIR=%PACKAGE%/Eigen.3.3.3/build/native/include
 set CAIRO_INCLUDE_DIRS=%PACKAGE%/cairo.1.12.18.0/build/native/include
@@ -20,7 +20,7 @@ mkdir %PACKAGE%\lib32
 
 cd %PACKAGE%
 
-%THISDIR%\nuget.exe install boost-vc140 -Version 1.62.00
+%THISDIR%\nuget.exe install boost-vc140 -Version 1.69.0
 %THISDIR%\nuget.exe install Eigen -Version 3.3.3
 %THISDIR%\nuget.exe install cairo -Version 1.12.18
 
@@ -50,6 +50,12 @@ call :find-files-64 %PACKAGE% "*atomic*.lib"
 call :find-files-64 %PACKAGE% "*iostreams*.dll"
 call :find-files-64 %PACKAGE% "*iostreams*.lib"
 
+call :find-files-64 %PACKAGE% "*zlib*.dll"
+call :find-files-64 %PACKAGE% "*zlib*.lib"
+
+call :find-files-64 %PACKAGE% "*bzip2*.dll"
+call :find-files-64 %PACKAGE% "*bzip2*.lib"
+
 call :find-files-32 %PACKAGE% "*regex*.dll" 
 call :find-files-32 %PACKAGE% "*regex*.lib"
 
@@ -74,13 +80,19 @@ call :find-files-32 %PACKAGE% "*atomic*.lib"
 call :find-files-32 %PACKAGE% "*iostreams*.dll"
 call :find-files-32 %PACKAGE% "*iostreams*.lib"
 
-@echo onc
+call :find-files-32 %PACKAGE% "*zlib*.dll"
+call :find-files-32 %PACKAGE% "*zlib*.lib"
+
+call :find-files-32 %PACKAGE% "*bzip2*.dll"
+call :find-files-32 %PACKAGE% "*bzip2*.lib"
+
+@echo on
 
 cd %THISDIR%
 mkdir build64
 cd build64
 
-cmake -G "Visual Studio 15 Win64" -DCMAKE_INSTALL_PREFIX=%P% -DRDK_BUILD_PYTHON_WRAPPERS=OFF -DRDK_BUILD_SWIG_WRAPPERS=ON -DRDK_BUILD_SWIG_JAVA_WRAPPER=OFF -DRDK_BUILD_SWIG_CSHARP_WRAPPER=ON -DBOOST_INCLUDEDIR="%BOOST_INCLUDEDIR%" -DBOOST_LIBRARYDIR="%BOOST_LIBRARYDIR%64" -DEIGEN3_INCLUDE_DIR="%EIGEN3_INCLUDE_DIR%" -DRDK_INSTALL_INTREE=OFF -DCPACK_INSTALL_PREFIX=rdkit -DRDK_BUILD_THREADSAFE_SSS=ON -DRDK_BUILD_AVALON_SUPPORT=ON -DRDK_BUILD_INCHI_SUPPORT=ON -DRDK_BUILD_CPP_TESTS=ON -DRDK_BUILD_CAIRO_SUPPORT=ON -DCAIRO_INCLUDE_DIRS="%CAIRO_INCLUDE_DIRS%" -DCAIRO_LIBRARIES="%CAIRO_LIBRARIES_64%" ..\..\rdkit
+cmake -G "Visual Studio 14 Win64" -DCMAKE_INSTALL_PREFIX=%P% -DRDK_BUILD_PYTHON_WRAPPERS=OFF -DRDK_BUILD_SWIG_WRAPPERS=ON -DRDK_BUILD_SWIG_JAVA_WRAPPER=OFF -DRDK_BUILD_SWIG_CSHARP_WRAPPER=ON -DBOOST_INCLUDEDIR="%BOOST_INCLUDEDIR%" -DBOOST_LIBRARYDIR="%BOOST_LIBRARYDIR%64" -DBoost_USE_STATIC_LIBS=ON -DEIGEN3_INCLUDE_DIR="%EIGEN3_INCLUDE_DIR%" -DRDK_INSTALL_INTREE=OFF -DCPACK_INSTALL_PREFIX=rdkit -DRDK_BUILD_THREADSAFE_SSS=ON -DRDK_BUILD_AVALON_SUPPORT=ON -DRDK_BUILD_INCHI_SUPPORT=ON -DRDK_BUILD_CPP_TESTS=ON -DRDK_BUILD_CAIRO_SUPPORT=ON -DCAIRO_INCLUDE_DIRS="%CAIRO_INCLUDE_DIRS%" -DCAIRO_LIBRARIES="%CAIRO_LIBRARIES_64%" ..\..\rdkit
 
 msbuild "ALL_BUILD.vcxproj" /m /p:PlatformTarget=x64 /p:Configuration=Release /maxcpucount:4 /t:Build
 
@@ -96,7 +108,7 @@ cd %THISDIR%
 mkdir build32
 cd build32
 
-cmake -G "Visual Studio 15" -DCMAKE_INSTALL_PREFIX=%P% -DRDK_BUILD_PYTHON_WRAPPERS=OFF -DRDK_BUILD_SWIG_WRAPPERS=ON -DRDK_BUILD_SWIG_JAVA_WRAPPER=OFF -DRDK_BUILD_SWIG_CSHARP_WRAPPER=ON -DBOOST_INCLUDEDIR="%BOOST_INCLUDEDIR%" -DBOOST_LIBRARYDIR="%BOOST_LIBRARYDIR%32" -DEIGEN3_INCLUDE_DIR="%EIGEN3_INCLUDE_DIR%" -DRDK_INSTALL_INTREE=OFF -DCPACK_INSTALL_PREFIX=rdkit -DRDK_BUILD_THREADSAFE_SSS=ON -DRDK_BUILD_AVALON_SUPPORT=ON -DRDK_BUILD_INCHI_SUPPORT=ON -DRDK_BUILD_CPP_TESTS=ON -DRDK_BUILD_CAIRO_SUPPORT=ON -DCAIRO_INCLUDE_DIRS="%CAIRO_INCLUDE_DIRS%" -DCAIRO_LIBRARIES="%CAIRO_LIBRARIES_32%" ..\..\rdkit
+cmake -G "Visual Studio 14" -DCMAKE_INSTALL_PREFIX=%P% -DRDK_BUILD_PYTHON_WRAPPERS=OFF -DRDK_BUILD_SWIG_WRAPPERS=ON -DRDK_BUILD_SWIG_JAVA_WRAPPER=OFF -DRDK_BUILD_SWIG_CSHARP_WRAPPER=ON -DBOOST_INCLUDEDIR="%BOOST_INCLUDEDIR%" -DBOOST_LIBRARYDIR="%BOOST_LIBRARYDIR%32" -DBoost_USE_STATIC-LIBS=ON -DEIGEN3_INCLUDE_DIR="%EIGEN3_INCLUDE_DIR%" -DRDK_INSTALL_INTREE=OFF -DCPACK_INSTALL_PREFIX=rdkit -DRDK_BUILD_THREADSAFE_SSS=ON -DRDK_BUILD_AVALON_SUPPORT=ON -DRDK_BUILD_INCHI_SUPPORT=ON -DRDK_BUILD_CPP_TESTS=ON -DRDK_BUILD_CAIRO_SUPPORT=ON -DCAIRO_INCLUDE_DIRS="%CAIRO_INCLUDE_DIRS%" -DCAIRO_LIBRARIES="%CAIRO_LIBRARIES_32%" ..\..\rdkit
 
 msbuild "ALL_BUILD.vcxproj" /m /p:PlatformTarget=x86 /p:Configuration=Release /maxcpucount:4 /t:Build
 
@@ -118,7 +130,7 @@ REM Copies files to the lib64 directory
 :find-files-64
     echo "Copying %~2"
     for /r "%~1" %%P in ("%~2") do (
-        echo %%~fP|find "address-model-64" >nul
+        echo %%~fP|find "x64" >nul
         if errorlevel 1 (call) else (copy  "%%~fP" "%~1"\lib64)          
     )
 goto :eof
@@ -127,7 +139,7 @@ REM Copies files to the lib32 directory
 :find-files-32
     echo "Copying %~2"
     for /r "%~1" %%P in ("%~2") do (
-        echo %%~fP|find "address-model-32" >nul
+        echo %%~fP|find "x32" >nul
         if errorlevel 1 (call) else (copy  "%%~fP" "%~1"\lib32)
     )
 goto :eof
